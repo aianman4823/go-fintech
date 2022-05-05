b8380ba916c9879c70b40fb93a4d60ff562158b0
aianman4823 <akitoaianman1@gmail.com>
2022-05-05 17:28:57 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"main/chapter13"
+)
+
+func main() {
+	fmt.Println("Main")
+	// chapter1.Import_func()
+	// chapter1.Variable()
+	// chapter1.Consts()
+	// chapter1.Exercise()
+	// chapter2.Main()
+	// chapter2.Log()
+	// chapter2.ErrorHandle()
+	// chapter2.Panic_recover()
+	// chapter2.Exercise()
+	// chapter3.Pointa()
+	// chapter3.New()
+	// chapter3.Struct()
+	// chapter3.Exercise()
+	// chapter4.Pointa_reciever()
+	// chapter4.Embedded()
+	// chapter4.Non_struct()
+	// chapter4.Interface()
+	// chapter4.Type_assert()
+	// chapter4.Stringer()
+	// chapter4.CustomError()
+	// chapter4.Exercise()
+	// chapter5.Gorutine()
+	// chapter5.Channel()
+	// chapter5.Bufferd_channel()
+	// chapter5.Bufferd_channel2()
+	// chapter5.Producer_consumer()
+	// chapter5.Fanout_fanin()
+	// chapter5.Select()
+	// chapter5.Select_default()
+	// chapter5.Mutex_pra()
+	// chapter5.Exercise()
+	// chapter6.Average()
+	// chapter12.Database()
+	chapter13.WebApp()
+}
