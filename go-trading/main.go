3b663c703bef0d0bd1aa14b33ecbf17f7cef0121
aianman4823 <akitoaianman1@gmail.com>
2022-05-07 19:24:54 +0900

    database作成

--- go-trading/main.go
+++ go-trading/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	bitflyer "go-trading/bitflyer"
+	"fmt"
+	models "go-trading/app/models"
 	config "go-trading/config"
 	utils "go-trading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	tickerChannel := make(chan bitflyer.Ticker)
-	apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+	fmt.Println(models.DbConnection)
 }
