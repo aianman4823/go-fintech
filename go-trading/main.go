245f577e6fa7091b8f4974b68c4a0d9ced6cce0d
aianman4823 <akitoaianman1@gmail.com>
2022-05-07 18:51:18 +0900

    RealTimeTickerを実装(うまく取れない

--- go-trading/main.go
+++ go-trading/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
 	bitflyer "go-trading/bitflyer"
 	config "go-trading/config"
 	utils "go-trading/utils"
-	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	ticker, _ := apiClient.GetTicker("BTC_USD")
-	fmt.Println(ticker)
-	fmt.Println(ticker.GetMidPrice())
-	fmt.Println(ticker.DateTime())
-	fmt.Println(ticker.TruncateDateTime(time.Hour))
+
+	tickerChannel := make(chan bitflyer.Ticker)
+	apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
 }
