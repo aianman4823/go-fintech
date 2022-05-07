3f6e271e30e15dfbcfe34e6c6c5c9f6f5b271ce9
aianman4823 <akitoaianman1@gmail.com>
2022-05-07 20:16:08 +0900

    webserverを作成

--- go-trading/main.go
+++ go-trading/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"go-trading/app/controllers"
-	models "go-trading/app/models"
 	config "go-trading/config"
 	utils "go-trading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
