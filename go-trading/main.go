cab6da6dc16b801b956d45a4ce4a46cf3507a1e4
aianman4823 <akitoaianman1@gmail.com>
2022-05-07 18:08:45 +0900

    ticker apiの追加

--- go-trading/main.go
+++ go-trading/main.go
@@ -5,10 +5,15 @@ import (
 	bitflyer "go-trading/bitflyer"
 	config "go-trading/config"
 	utils "go-trading/utils"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
