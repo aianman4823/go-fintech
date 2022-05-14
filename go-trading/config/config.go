cccbd813f860ee3e5b3200502b51ce634414b022
aianman4823 <akitoaianman1@gmail.com>
2022-05-14 17:30:10 +0900

    設定ファイル変更

--- go-trading/config/config.go
+++ go-trading/config/config.go
@@ -19,6 +19,12 @@ type ConfigList struct {
 	DbName        string
 	SQLDriver     string
 	Port          int
+
+	BackTest         bool
+	UsePercent       float64
+	DataLimit        int
+	StopLimitPercent float64
+	NumRanking       int
 }
 
 var Config ConfigList
@@ -45,6 +51,12 @@ func init() {
 		TradeDuration: durations[cfg.Section("gotrading").Key("trade_duration").String()],
 		DbName:        cfg.Section("db").Key("name").String(),
 		SQLDriver:     cfg.Section("db").Key("driver").String(),
-		Port:        cfg.Section("web").Key("port").MustInt(),
+		Port:          cfg.Section("web").Key("port").MustInt(),
+
+		BackTest:         cfg.Section("gotrading").Key("back_test").MustBool(),
+		UsePercent:       cfg.Section("gotrading").Key("use_percent").MustFloat64(),
+		DataLimit:        cfg.Section("gotrading").Key("data_limit").MustInt(),
+		StopLimitPercent: cfg.Section("gotrading").Key("stop_limit_percent").MustFloat64(),
+		NumRanking:       cfg.Section("gotrading").Key("num_ranking").MustInt(),
 	}
 }
